57444b0a39126fc71cf3d05e61d56fb751716d72
Ambrin Chaudhary <ambrin.chaudhary@liferay.com>
2016-10-27 13:25:20 +0200

    LPS-68550 Extract render logic to BaseKBPortlet

--- modules/apps/knowledge-base/knowledge-base-web/src/main/java/com/liferay/knowledge/base/web/internal/portlet/ArticlePortlet.java
+++ modules/apps/knowledge-base/knowledge-base-web/src/main/java/com/liferay/knowledge/base/web/internal/portlet/ArticlePortlet.java
@@ -28,10 +28,8 @@ import com.liferay.portal.kernel.security.permission.PermissionChecker;
 import com.liferay.portal.kernel.servlet.SessionErrors;
 import com.liferay.portal.kernel.servlet.SessionMessages;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
-import com.liferay.portal.kernel.util.Constants;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
-import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 
 import java.io.IOException;
@@ -81,7 +79,7 @@ import org.osgi.service.component.annotations.Reference;
 public class ArticlePortlet extends BaseKBPortlet {
 
 	@Override
-	public void render(
+	public void doRender(
 			RenderRequest renderRequest, RenderResponse renderResponse)
 		throws IOException, PortletException {
 
@@ -97,12 +95,6 @@ public class ArticlePortlet extends BaseKBPortlet {
 			if (resourcePrimKey > 0) {
 				kbArticle = kbArticleService.getLatestKBArticle(
 					resourcePrimKey, WorkflowConstants.STATUS_APPROVED);
-			}
-
-			String cmd = ParamUtil.getString(renderRequest, Constants.CMD);
-
-			if (Validator.isNotNull(cmd) && cmd.equals("compareVersions")) {
-				super.compareVersions(renderRequest);
 			}
 
 			renderRequest.setAttribute(
@@ -127,8 +119,6 @@ public class ArticlePortlet extends BaseKBPortlet {
 				throw new PortletException(e);
 			}
 		}
-
-		super.render(renderRequest, renderResponse);
 	}
 
 	@Override
